50337c91fd42a4f3826858988172727bd7961dbf
Garvit9000c <909garvit@gmail.com>
2021-01-30 14:46:48 +0530

    Gui v.1.5

--- gui/quiz.py
+++ gui/quiz.py
@@ -8,37 +8,38 @@ def loginPage(logdata):
     sup.destroy()
     global login
     login = Tk()
+    login.title("-Texam-")
     
     user_name = StringVar()
     password = StringVar()
     
-    login_canvas = Canvas(login,width=720,height=440,bg="blue")
+    login_canvas = Canvas(login,width=720,height=440,bg="white")
     login_canvas.pack()
 
     login_frame = Frame(login_canvas,bg="white")
     login_frame.place(relwidth=0.8,relheight=0.8,relx=0.1,rely=0.1)
 
-    heading = Label(login_frame,text="Quiz App Login",fg="black",bg="white")
+    heading = Label(login_frame,text="Login",fg="black",bg="white")
     heading.config(font=('calibri 40'))
-    heading.place(relx=0.2,rely=0.1)
+    heading.place(relx=0.4,rely=0.1)
 
     #USER NAME
     ulabel = Label(login_frame,text="Username",fg='black',bg='white')
-    ulabel.place(relx=0.21,rely=0.4)
+    ulabel.place(relx=0.15,rely=0.4)
     uname = Entry(login_frame,bg='#d3d3d3',fg='black',textvariable = user_name)
     uname.config(width=42)
     uname.place(relx=0.31,rely=0.4)
 
     #PASSWORD
     plabel = Label(login_frame,text="Password",fg='black',bg='white')
-    plabel.place(relx=0.215,rely=0.5)
+    plabel.place(relx=0.15,rely=0.5)
     pas = Entry(login_frame,bg='#d3d3d3',fg='black',show="*",textvariable = password)
     pas.config(width=42)
     pas.place(relx=0.31,rely=0.5)
 
     def check():
         for a,b,c,d in logdata:
-            if b == uname.get() and c == pas.get():
+            if b == uname.get() and c == pas.get() and b!="" and c!=0:
                 menu()
                 break
         else:
@@ -47,9 +48,8 @@ def loginPage(logdata):
     
     #LOGIN BUTTON
     log = Button(login_frame,text='Login',padx=5,pady=5,width=5,command=check)
-    log.configure(width = 15,height=1, activebackground = "#33B5E5", relief = FLAT)
+    log.configure(width = 15,height=1, activebackground = "#33B5E5", relief = FLAT,bg="green")
     log.place(relx=0.4,rely=0.6)
-    
     
     login.mainloop()
 
@@ -57,33 +57,33 @@ def signUpPage():
     root.destroy()
     global sup
     sup = Tk()
-    
+    sup.title("-Texam-")
     fname = StringVar()
     uname = StringVar()
     passW = StringVar()
     country = StringVar()
     
     
-    sup_canvas = Canvas(sup,width=720,height=440,bg="blue")
+    sup_canvas = Canvas(sup,width=720,height=440,bg="white")
     sup_canvas.pack()
 
     sup_frame = Frame(sup_canvas,bg="white")
-    sup_frame.place(relwidth=0.8,relheight=0.8,relx=0.1,rely=0.1)
+    sup_frame.place(relwidth=0.7,relheight=0.8,relx=0.1,rely=0.1)
 
-    heading = Label(sup_frame,text="Quiz App SignUp",fg="black",bg="white")
+    heading = Label(sup_frame,text="SignUp",fg="black",bg="white")
     heading.config(font=('calibri 40'))
-    heading.place(relx=0.2,rely=0.1)
+    heading.place(relx=0.4,rely=0.1)
 
     #full name
     flabel = Label(sup_frame,text="Full Name",fg='black',bg='white')
-    flabel.place(relx=0.21,rely=0.4)
+    flabel.place(relx=0.15,rely=0.4)
     fname = Entry(sup_frame,bg='#d3d3d3',fg='black',textvariable = fname)
     fname.config(width=42)
     fname.place(relx=0.31,rely=0.4)
 
     #username
     ulabel = Label(sup_frame,text="Username",fg='black',bg='white')
-    ulabel.place(relx=0.21,rely=0.5)
+    ulabel.place(relx=0.15,rely=0.5)
     user = Entry(sup_frame,bg='#d3d3d3',fg='black',textvariable = uname)
     user.config(width=42)
     user.place(relx=0.31,rely=0.5)
@@ -91,7 +91,7 @@ def signUpPage():
     
     #password
     plabel = Label(sup_frame,text="Password",fg='black',bg='white')
-    plabel.place(relx=0.215,rely=0.6)
+    plabel.place(relx=0.15,rely=0.6)
     pas = Entry(sup_frame,bg='#d3d3d3',fg='black',show="*",textvariable = passW)
     pas.config(width=42)
     pas.place(relx=0.31,rely=0.6)
@@ -100,7 +100,7 @@ def signUpPage():
     
     #country
     clabel = Label(sup_frame,text="Country",fg='black',bg='white')
-    clabel.place(relx=0.215,rely=0.7)
+    clabel.place(relx=0.15,rely=0.7)
     c = Entry(sup_frame,bg='#d3d3d3',fg='black',textvariable = country)
     c.config(width=42)
     c.place(relx=0.31,rely=0.7)
@@ -132,11 +132,11 @@ def signUpPage():
     #signup BUTTON
     sp = Button(sup_frame,text='SignUp',padx=5,pady=5,width=5,command = addUserToDataBase,bg='green')
     sp.configure(width = 15,height=1, activebackground = "#33B5E5", relief = FLAT)
-    sp.place(relx=0.4,rely=0.8)
+    sp.place(relx=0.45,rely=0.8)
 
-    log = Button(sup_frame,text='Already have a Account?',padx=5,pady=5,width=5,command = gotoLogin,bg="white",fg='blue')
-    log.configure(width = 16,height=1, activebackground = "#33B5E5", relief = FLAT)
-    log.place(relx=0.4,rely=0.9)
+    log = Button(sup_frame,text='Login',padx=5,pady=5,width=5,command = gotoLogin,bg="white",fg='blue')
+    log.configure(width = 15,height=1, activebackground = "#33B5E5", relief = FLAT)
+    log.place(relx=0.45,rely=0.9)
 
     sup.mainloop()
 
