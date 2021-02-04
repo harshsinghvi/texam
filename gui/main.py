4e80b8cee22af9ad5baf23c236e9101ffea23d96
Garvit9000c <909garvit@gmail.com>
2021-02-04 18:06:55 +0530

    Update Zero

--- gui/main.py
+++
@@ -1,7 +0,0 @@
-import os
-from multiprocessing import Pool
-processes = ('quiz.py', 'audio.py')
-def run_process(process):
-    os.system('python {}'.format(process))
-pool = Pool(processes=2)
-pool.map(run_process, processes)
