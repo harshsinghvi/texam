3cc594f870f0e1b785ac43fa7276e5bdd3b573cb
Harsh <root@harshsinghvi.com>
2021-02-18 14:01:03 +0530

    update auth

--- keygen.py
+++ keygen.py
@@ -5,10 +5,7 @@ from base64 import b64encode
 new_key  = b64encode(urandom(12)).decode('utf-8')
 
 
-
-
 MONGODB_URI = getenv("MONGO_DB_URI")
-
 
 
 db = pymongo.MongoClient(MONGODB_URI).texam
@@ -16,8 +13,8 @@ s= db.texam.find_one()
 r  = db.texam.update_one({"_id":s["_id"]}, { "$set": { "auth": new_key } }).acknowledged
 
 if r :
-    f = open('key.py','w')
-    f.write("AUTH = \"{}\"".format(new_key))
+    f = open('key.txt','w')
+    f.write(new_key)
     f.close()
     print("OK")
 else :
